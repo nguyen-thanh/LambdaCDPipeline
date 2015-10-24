64cc2c4bd068b8919ffb015ac235e98a26b960f7
Thanh Nguyen <nguyenthanh11190@gmail.com>
2015-10-24 22:46:27 +0000

    Upload to s3 after checkout

--- aws_modules/stage/checkout/index.js
+++ aws_modules/stage/checkout/index.js
@@ -3,11 +3,13 @@
  */
 
  var action = require('../../../lib/checkout');
+ var s3upload = require('../../../lib/s3upload');
 
 // Export For Lambda Handler
 module.exports.run = function(event, context, cb) {
   console.log(event.url);
   return action(event.url)
+    .then(s3upload)
     .then(function(result) {
       cb(null, result);
     })
